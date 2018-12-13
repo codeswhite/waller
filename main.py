f0c6bb7de20a94e7288b5a9b6981831cb794e671
WhiteOnBlackCode <max3227@gmail.com>
2018-12-13 02:05:48 +0200

    + Added ability to set the DM's lock screen background image via pressing the [L] key. * Only LDM's GTK greeter is supported for now(!) * Major refactoring and break-apart of the main procedure

--- main.py
+++ main.py
@@ -8,7 +8,6 @@
 3 == Running as root (don't)
 
 TODO:
-* Add ability to set the LDM lock screen wallpaper (possibly by pressing a specified key)
 * Add support for multiple LDM greeters (and DMs)
 """
 
@@ -17,11 +16,12 @@ import os
 import stat
 from random import randint
 from subprocess import check_output, call
-from typing import List
+from typing import List, Generator, Tuple
 
 from utils import banner, pr, is_image
 
 DIRECTORY = '/home/maximus/wallpapers'
+LDM_GTK_CONF = '/etc/lightdm/lightdm-gtk-greeter.conf'
 
 
 def clear_win(win) -> None:
@@ -38,7 +38,6 @@ def get_cmd(monitor_id: int) -> List[str]:
     """
     The system command which will return path of monitor's wallpaper
     """
-
     return ['xfconf-query', '-c', 'xfce4-desktop', '-p',
             '/backdrop/screen0/monitor%d/workspace0/last-image' % monitor_id]
 
@@ -50,9 +49,100 @@ def apply(name, mon_id) -> None:
     :param name: Image name in the directory
     :param mon_id: Desired monitor ID
     """
-
     cmd = get_cmd(mon_id) + ['-s', os.path.join(DIRECTORY, name)]
     call(cmd)
+
+
+def get_ldm_bg() -> str:
+    """
+    Fetch the background image of the LDM's GTK greeter from the config file
+    :return: Full path to the background image
+    """
+    with open(LDM_GTK_CONF) as ldm_file:
+        for line in ldm_file:
+            if line.startswith('background'):
+                return line.strip().split(' ')[2]
+    raise LookupError("[ERR] Couldn't find the LDM greeter's background!")
+
+
+def set_ldm_bg(win, ldm_bg_name, wall_name) -> None:
+    """
+    Set the background image of the LDM's GTK greeter in the config file
+    """
+    if ldm_bg_name == wall_name:
+        win.addstr('[X] Cannot change DM background to the same one!\n', curses.color_pair(2))
+        win.getkey()
+    else:
+        win.addstr('[!] Replacement of the lock-screen background requires elevation\n',
+                   curses.color_pair(5))
+        call("sudo sed -i 's/%s/%s/g' " % (ldm_bg_name, wall_name) + LDM_GTK_CONF, shell=True)
+        win.addstr('[+] Lock-screen background replaced!\n', curses.color_pair(3))
+        win.getkey()
+
+
+def reset_permissions(avail: tuple, ldm_bg_path: str) -> None:
+    """
+    Sets proper permissions [400] for all the images
+     and [404] for the DM background image
+    :param avail: Available images
+    :param ldm_bg_path: DM's background image path
+    """
+    for wall in avail:
+        path = os.path.join(DIRECTORY, wall)
+        perm = stat.S_IRUSR
+        if path == ldm_bg_path:
+            perm |= stat.S_IROTH
+        os.chmod(path, perm)
+
+
+def collect_available() -> Generator[str, str, None]:
+    """
+    Collect available images in the specified directory
+    :return: File-names
+    """
+    for wall in os.listdir(DIRECTORY):
+        if is_image(os.path.join(DIRECTORY, wall)):
+            yield wall
+
+
+def collect_monitors() -> Generator[str, str, None]:
+    """
+    Collect connected monitors, via xRandr
+    :return: Monitor names
+    """
+    for line in check_output('xrandr').decode().split('\n'):
+        if 'connected' in line:
+            yield line.split(' ')[0]
+
+
+def get_current_wall(win, monitor_id: int, available: tuple) -> Tuple[str, int]:
+    """
+    Find currently used wallpaper for the specific monitor-ID and return its name,
+    as well as the index of the file in the directory
+
+    :param win: Curses window handle
+    :param monitor_id: Specified monitor
+    :param available: Available images
+    :return: A tuple of (IMG-NAME, IMG-ID)
+    """
+    stream = bytes(check_output(get_cmd(monitor_id)))
+    name = os.path.split(stream.strip().decode())[1]
+
+    try:
+        return name, available.index(name)
+
+    except ValueError:
+        win.addstr('[X] Current wall "%s" not found in %s\n' %
+                   (name, DIRECTORY), curses.color_pair(2))
+        win.addstr('[+] Press [R] to reset to the first image', curses.color_pair(5))
+        if win.getkey() != 'r':
+            exit(1)
+
+        # Set current to first
+        current_id = 0
+        name = available[current_id]
+        apply(name, monitor_id)
+        return name, current_id
 
 
 def main(win) -> None:
@@ -67,51 +157,25 @@ def main(win) -> None:
     curses.init_pair(5, curses.COLOR_YELLOW, -1)
 
     # Get 'LDM GTK greeter' wallpaper
-    ldm = None
-    with open('/etc/lightdm/lightdm-gtk-greeter.conf') as file:
-        for line in file:
-            if line.startswith('background'):
-                ldm = line.strip().split(' ')[2]
+    ldm_bg_path = get_ldm_bg()
 
-    # Get available & set permissions
-    available = []
-    for wall in os.listdir(DIRECTORY):
-        path = os.path.join(DIRECTORY, wall)
-        if is_image(path):
-            available.append(wall)
-            perm = stat.S_IRUSR
-            if path == ldm:
-                perm |= stat.S_IROTH
-            os.chmod(path, perm)
+    # Get available
+    available = tuple(collect_available())
+
+    # Set permissions
+    reset_permissions(available, ldm_bg_path)
 
     # Check monitors
     mon_id = 0
-    mons = []
-    for line in check_output('xrandr').decode().split('\n'):
-        if 'connected' in line:
-            mons.append(line.split(' ')[0])
+    mons = tuple(collect_monitors())
 
-    while 1:  # Monitor l00p
+    while 1:  # Monitor Loop
         clear_win(win)
 
         # Get current wallpaper
-        stream = bytes(check_output(get_cmd(mon_id)))
-        current = stream.strip().decode()
-        current_name = os.path.split(current)[1]
+        current_name, current_id = get_current_wall(win, mon_id, available)
 
-        try:
-            cid = available.index(current_name)
-        except ValueError:
-            win.addstr('[X] Current wall "%s" not found in %s\n' %
-                       (current_name, DIRECTORY), curses.color_pair(2))
-            win.addstr('[+] Press [R] to reset to the first image', curses.color_pair(5))
-            if win.getkey() != 'r':
-                exit(1)
-            cid = 0
-            current = available[cid]
-            apply(current, mon_id)
-
-        while 1:
+        while 1:  # Inner Loop
             clear_win(win)
 
             # show info
@@ -119,8 +183,8 @@ def main(win) -> None:
                 win.addstr('[+] Using monitor: ')
                 win.addstr('%s\n' % mons[mon_id], curses.color_pair(3))
             win.addstr('[+] Current wall: ')
-            win.addstr('%s (%d/%d)' % (os.path.split(current)[1], cid + 1,
-                                       len(available)), curses.color_pair(3))
+            win.addstr('%s (%d/%d)\n' % (current_name, current_id + 1,
+                                         len(available)), curses.color_pair(3))
 
             key = str(win.getkey()).lower()
             if not key:
@@ -134,19 +198,25 @@ def main(win) -> None:
                 break  # Back to monitor loop
 
             if key == 'r':
-                cid = randint(0, len(available))
+                current_id = randint(0, len(available))
             elif key == 'key_left':
-                cid -= 1
-                if cid < 0:
-                    cid = len(available) - 1
+                current_id -= 1
+                if current_id < 0:
+                    current_id = len(available) - 1
             elif key == 'key_right':
-                cid += 1
-                if cid >= len(available):
-                    cid = 0
+                current_id += 1
+                if current_id >= len(available):
+                    current_id = 0
+
+            # DM background
+            elif key == 'l':
+                set_ldm_bg(win, os.path.split(ldm_bg_path)[1], available[current_id])
+                ldm_bg_path = get_ldm_bg()
+                reset_permissions(available, ldm_bg_path)
+                continue
 
             # Application
-            current = available[cid]
-            apply(current, mon_id)
+            apply(available[current_id], mon_id)
 
 
 if __name__ == '__main__':
